d7870ee9747e74f748e79607c2548d73d86bd0c5
Belen Montoya <mariabmontoya@gmail.com>
2021-06-22 21:26:44 -0300

    feature (agregar menu de opciones)

--- Martes_prog/Program.cs
+++ Martes_prog/Program.cs
@@ -12,7 +12,7 @@ namespace Martes_prog
         static void Main(string[] args)
         {
 
-            string[,] matrizNombres = new string[2,3];
+            string[,] matrizNombres = new string[2, 3];
 
             int[] arrayEdades = new int[5];
 
@@ -24,6 +24,73 @@ namespace Martes_prog
             for (int i = 0; i < arrayEdades.Length; i++)
             {
                 Console.WriteLine($"{arrayEdades[i]}");
+            }
+
+            static void Main(string[] args)
+            {
+                while (MainMenu())
+                {
+                }
+            }
+
+            /// <summary>
+            /// Funcion generica para mostrar menues
+            /// </summary>
+            /// <param name="ShowMenu"></param>
+            /// <returns></returns>
+            static void ShowMenu(string[] menu)
+            {
+                int ind = 1;
+                for (var i = 0; i < menu.Length; i++)
+                {
+                    Console.WriteLine($"{ind}) {menu[i]}");
+                    ind++;
+                }
+
+            }
+
+            static void MenuHeading(string text)
+            {
+                Console.WriteLine(text);
+                Console.WriteLine("------------------------------------");
+            }
+
+            /// <summary>
+            /// Fucion que maneja el menu principal
+            /// </summary>
+            /// <returns></returns>
+            static bool MainMenu()
+            {
+                Console.Clear();
+
+                MenuHeading("Bienvenidx! Elija una opción para continuar:");
+
+                string[] menu =
+                    {
+                                    "OP 1",
+                                    "OP 2",
+                                    "OP 3",
+                                    "OP 4"
+                                };
+
+                ShowMenu(menu);
+
+                Console.Write("\r\nSeleccionar una opción: ");
+
+                //switch de menu
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        return true;
+                    case "2":
+                        return true;
+                    case "3":
+                        return true;
+                    case "4":
+                        return false;
+                    default:
+                        return true;
+                }
             }
 
 
